348ad7fec3479c7a51071f8243e7b3aef1a8aec9
leetaewoo <taewoo5131@gmail.com>
2021-10-16 15:19:44 +0900

    stack finish commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,25 @@ public class Main {
 
     public static void main(String[] args) {
         Stack stack = new Stack(5);
-        stack.makeStack();
+        String array = stack.makeStack(); // 만들기
+        stack.put(1);
+        stack.printToStack();
+        stack.put(2);
+        stack.printToStack();
+        stack.put(3);
+        stack.printToStack();
+        stack.put(4);
+        stack.printToStack();
+        stack.put(5);
+        stack.printToStack();
+        stack.put(6);
+        stack.printToStack();
+        System.out.println("put over !!!!");
+        stack.pop();
+        stack.printToStack();
+        stack.pop();
+        stack.printToStack();
+        stack.clearStack();
+        stack.printToStack();
     }
 }
--- src/com/company/stack/Stack.java
+++ src/com/company/stack/Stack.java
@@ -2,15 +2,64 @@ package com.company.stack;
 
 public class Stack {
     private int stackSize;
+    private int[] array;
+    private int top;
 
     public Stack(){}
     public Stack(int size){
         this.stackSize = size;
     }
 
-    public void makeStack(){
+    public String makeStack(){
         System.out.println("stack!");
-        int[] array = new int[this.stackSize];
+        array = new int[this.stackSize];
+        top = -1;
+        return array.toString();
     }
+
+    // stack is empty?
+    public boolean isEmpty(){
+        return (top == -1);
+    }
+
+    public void printToStack(){
+        for(int i = 0; i < array.length; i++){
+            System.out.print(array[i] + " ");
+        }
+        System.out.println("");
+    }
+
+    public void put(int n){
+        if(this.top + 1 == stackSize){
+            System.out.println("stack is full !!!!");
+            return;
+        }
+        if(isEmpty()){
+            array[0] = n;
+            this.top = 0;
+        }else{
+            array[this.top + 1] = n;
+            this.top = this.top + 1;
+        }
+    }
+
+    public void pop(){
+        if(isEmpty()){
+            System.out.println("ERROR : the stack is Empty !! impossible pop this stack.");
+        }else{
+            System.out.println(array[this.top]);
+            array[this.top] = 0;
+            this.top = this.top - 1;
+        }
+    }
+
+    public void clearStack(){
+        for(int i = 0; i < array.length; i++){
+            array[i] = 0;
+        }
+        System.out.println("stack clear !!");
+    }
+
+
     
 }
