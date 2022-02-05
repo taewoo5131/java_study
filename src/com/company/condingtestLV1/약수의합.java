2b0cd527275395c396f37a53f36e1a2eaa560785
leetaewoo <taewoo5131@gmail.com>
2022-02-05 16:14:34 +0900

    코딩테스트 1

---
+++ src/com/company/condingtestLV1/약수의합.java
@@ -0,0 +1,26 @@
+package com.company.condingtestLV1;
+
+/**
+ * 정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
+ *
+ * 제한 사항
+ * n은 0 이상 3000이하인 정수입니다.
+ */
+public class 약수의합 {
+    public static void main(String[] args) {
+        Solution.solution(5);
+    }
+    private static class Solution {
+        static public int solution(int n) {
+            int answer = 0;
+            if(n > -1 && n < 3001){
+                for (int i = 1; i <= n; i++) {
+                    if (n % i == 0) {
+                        answer += i;
+                    }
+                }
+            }
+            return answer;
+        }
+    }
+}
