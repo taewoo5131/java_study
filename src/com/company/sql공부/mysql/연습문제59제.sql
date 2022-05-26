81c74e2a07e555428c2e984563f788e8b107ea66
leetaewoo <taewoo5131@gmail.com>
2022-05-26 23:19:52 +0900

    [sql 공부 - 59제] - 연습문제 20번까지 풀이

---
+++ src/com/company/sql공부/mysql/연습문제59제.sql
@@ -0,0 +1,295 @@
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+-- DML
+CREATE TABLE dept (
+	deptno               int  NOT NULL  AUTO_INCREMENT,
+	dname                varchar(20)    ,
+	loc                  varchar(20)    ,
+	CONSTRAINT pk_dept PRIMARY KEY ( deptno )
+ ) engine=InnoDB;
+
+CREATE TABLE emp (
+	empno                int  NOT NULL  AUTO_INCREMENT,
+	ename                varchar(20)    ,
+	job      			 varchar(20),
+	mgr                  smallint    ,
+	hiredate             date    ,
+	sal                  numeric(7,2)    ,
+	comm                 numeric(7,2)    ,
+	deptno               int    ,
+	CONSTRAINT pk_emp PRIMARY KEY ( empno )
+ ) engine=InnoDB;
+
+ALTER TABLE emp ADD CONSTRAINT fk_emp_dept FOREIGN KEY ( deptno ) REFERENCES dept( deptno ) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- DDL
+insert into dept values(10, 'ACCOUNTING', 'NEW YORK');
+insert into dept values(20, 'RESEARCH', 'DALLAS');
+insert into dept values(30, 'SALES', 'CHICAGO');
+insert into dept values(40, 'OPERATIONS', 'BOSTON');
+
+insert into emp values( 7839, 'KING', 'PRESIDENT', null, STR_TO_DATE ('17-11-1981','%d-%m-%Y'), 5000, null, 10);
+insert into emp values( 7698, 'BLAKE', 'MANAGER', 7839, STR_TO_DATE('1-5-1981','%d-%m-%Y'), 2850, null, 30);
+insert into emp values( 7782, 'CLARK', 'MANAGER', 7839, STR_TO_DATE('9-6-1981','%d-%m-%Y'), 2450, null, 10);
+insert into emp values( 7566, 'JONES', 'MANAGER', 7839, STR_TO_DATE('2-4-1981','%d-%m-%Y'), 2975, null, 20);
+insert into emp values( 7788, 'SCOTT', 'ANALYST', 7566, DATE_ADD(STR_TO_DATE('13-7-1987','%d-%m-%Y'),INTERVAL -85 DAY)  , 3000, null, 20);
+insert into emp values( 7902, 'FORD', 'ANALYST', 7566, STR_TO_DATE('3-12-1981','%d-%m-%Y'), 3000, null, 20);
+insert into emp values( 7369, 'SMITH', 'CLERK', 7902, STR_TO_DATE('17-12-1980','%d-%m-%Y'), 800, null, 20);
+insert into emp values( 7499, 'ALLEN', 'SALESMAN', 7698, STR_TO_DATE('20-2-1981','%d-%m-%Y'), 1600, 300, 30);
+insert into emp values( 7521, 'WARD', 'SALESMAN', 7698, STR_TO_DATE('22-2-1981','%d-%m-%Y'), 1250, 500, 30);
+insert into emp values( 7654, 'MARTIN', 'SALESMAN', 7698, STR_TO_DATE('28-09-1981','%d-%m-%Y'), 1250, 1400, 30);
+insert into emp values( 7844, 'TURNER', 'SALESMAN', 7698, STR_TO_DATE('8-9-1981','%d-%m-%Y'), 1500, 0, 30);
+insert into emp values( 7876, 'ADAMS', 'CLERK', 7788, DATE_ADD(STR_TO_DATE('13-7-1987', '%d-%m-%Y'),INTERVAL -51 DAY), 1100, null, 20);
+insert into emp values( 7900, 'JAMES', 'CLERK', 7698, STR_TO_DATE('3-12-1981','%d-%m-%Y'), 950, null, 30);
+insert into emp values( 7934, 'MILLER', 'CLERK', 7782, STR_TO_DATE('23-1-1982','%d-%m-%Y'), 1300, null, 10);
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+--------- 문제
+
+-- 1. 덧셈연산자를 이용하여 모든 사원에 대해서 $300의 급여 인상을 계산한 후 사원의 이름, 급여, 인상된 급여를 출력하시오.
+select ename, sal, sal+300 as plSal from emp
+
+-- 2. 사원의 이름, 급여, 연간 총 수입을 총 수입이 많은 것부터 작은 순으로 출력하시오, 연간 총수입은 월급에 12를 곱한 후 $100의 상여금을 더해서 계산하시오.
+select ename , sal , sal * 12 + 100 as 연간총수입
+from emp
+order by 연간총수입 desc
+
+-- 3. 급여가 2000을 넘는 사원의 이름과 급여를 표현, 급여가 많은 것부터 작은 순으로 출력하시오.
+select ename, sal
+from emp
+where sal > 2000
+order by sal DESC
+
+-- 4. 사원번호가 7788인 사원의 이름과 부서번호를 출력하시오.
+select ename, deptno
+from emp
+where empno = 7788
+
+-- 5. 급여가 2000에서 3000 사이에 포함되지 않는 사원의 이름과 급여를 출력하시오.
+select ename , sal
+from emp
+where sal BETWEEN 2000 AND 3000
+
+-- 6. 1981년 2월 20일 부터 1981년 5월 1일 사이에 입사한 사원의 이름, 담당업무, 입사일을 출력하시오.
+select ename, job, hiredate
+from emp
+where hiredate BETWEEN '1981/02/20' AND '1981/05/01'
+
+-- 7. 부서번호가 20 및 30에 속한 사원의 이름과 부서번호를 출력, 이름을 기준(내림차순)으로 영문자순으로 출력하시오.
+select ename , deptno
+from emp
+where deptno IN(20,30)
+order by ename DESC
+
+-- 8. 사원의 급여가 2000에서 3000사이에 포함되고 부서번호가 20 또는 30인 사원의 이름, 급여와 부서번호를 출력, 이름순(오름차순)으로 출력하시오.
+SELECT ename , sal ,deptno
+from emp
+where (sal BETWEEN 2000 and 3000)
+and deptno IN(20,30)
+order by ename asc
+
+-- 9. 1981년도에 입사한 사원의 이름과 입사일을 출력하시오. (like 연산자와 와일드카드 사용)
+select ename , hiredate
+from emp
+where hiredate LIKE '1981%'
+
+-- 10. 관리자가 없는 사원의 이름과 담당 업무를 출력하시오.
+select ename , deptno
+from emp
+where ISNULL(mgr)
+
+-- 11. 커미션을 받을 수 있는 자격이 되는 사원의 이름, 급여, 커미션을 출력하되 급여 및 커미션을 기준으로 내림차순 정렬하여 표시하시오.
+select ename , sal , comm
+from emp
+where comm IS NOT NULL
+order by comm desc
+
+-- 12. 이름의 세번째 문자가 R인 사원의 이름을 표시하시오.
+select ename
+from emp
+where SUBSTR(ename,3,1) = 'R'
+
+-- 13. 이름에 A와 E를 모두 포함하고 있는 사원의 이름을 표시하시오
+select ename
+from emp
+where ename like ('%A%') and ename like ('%E%')
+
+-- 14. 담당업무가 CLERK, 또는 SALESMAN이면서 급여가 $1600, $950 또는 $1300이 아닌 사원의 이름, 담당업무, 급여를 출력하시오.
+select ename, job , sal
+from emp
+where (job = 'CLERK'
+or job = 'SALESMAN') AND sal not in (1600, 950, 1300)
+
+-- 15. 커미션이 $500 이상인 사원의 이름과 급여 및 커미션을 출력하시오
+select ename ,sal ,comm
+from emp
+where comm >= 500
+
+-- 16. SUBSTR 함수를 사용하여 사원들의 입사한 년도와 입사한 달만 출력하시오.
+select ename , SUBSTR(hiredate,1,4) as 입사한년도, SUBSTR(hiredate,6,2) as 입사한달 from emp
+
+-- 17. SUBSTR 함수를 사용하여 4월에 입사한 사원을 출력하시오.
+select ename
+ from emp
+ where SUBSTR(hiredate,6,2) = '04'
+
+-- 18. MOD 함수를 사용하여 사원번호가 짝수인 사람만 출력하시오.
+select ename
+ from emp
+ where MOD(empno,2) = 0
+
+-- 19. 입사일을 년도는 2자리(YY), 월은 숫자(MON)로 표시하고 요일은 약어 (DY)로 지정하여 출력하시오.
+select SUBSTR(hiredate,3,2) 년도 , SUBSTR(hiredate,6,2) DY  from emp
+
+-- 20. 올해 몇 칠이 지났는지 출력하시오. 현재날짜에서 올해 1월 1일을 뺀 결과를 출력하고 TO_DATE 함수를 사용하여 데이터 형을 일치 시키시오.
+SELECT DATEDIFF(NOW(),DATE('2022-01-01'))
+
+-- 21. 사원들의 상관 사번을 출력하되 상관이 없는 사원에 대해서는 NULL 값 대신 0으로 출력하시오.
+
+-- 22. DECODE 함수로 직급에 따라 급여를 인상하도록 하시오. 직급이 ‘ANALIST”인 사원은 200, ‘SALESMAN’인 사원은 180, ‘MANAGER’인 사원은 150, ‘CLERK”인 사원은 100을 인상하시오.
+
+-- 23. 모든 사원의 급여 최고액, 최저액, 총액 및 평균 급여를 출력하시오. 평균에 대해서는 정수로 반올림하시오.
+
+-- 24. 각 담당 업무 유형별로 급여 최고액, 최저액, 총액 및 평균 액을 출력하시오. 평균에 대해서는 정수로 반올림 하시오.
+
+-- 25. COUNT(*) 함수를 이용하여 담당업무가 동일한 사원 수를 출력하시오.
+
+-- 26. 관리자 수를 나열하시오.
+
+-- 27. 급여 최고액, 급여 최저액의 차액을 출력하시오.
+
+-- 28. 직급별 사원의 최저 급여를 출력하시오. 관리자를 알 수 없는 사원의 최저 급여가 2000 미만인 그룹은 제외시키고 결과를 급여에 대한 내림차순으로 정렬하여 출력하시오.
+
+-- 29. 각 부서에 대해 부서번호, 사원 수, 부서 내의 모든 사원의 평균 급여를 출력하시오. 평균 급여는 소수점 둘째 자리로 반올림 하시오.
+
+-- 30. 각 부서에 대해 부서번호 이름, 지역 명, 사원 수, 부서내의 모든 사원의 평균 급여를 출력하시오. 평균 급여는 정수로 반올림 하시오. DECODE 사용.
+
+-- 31. 업무를 표시한 다음 해당 업무에 대해 부서 번호별 급여 및 부서 10, 20, 30의 급여 총액을 각각 출력하시오.
+--     별칭은 각 job, dno, 부서 10, 부서 20, 부서 30, 총액으로 지정하시오. ( hint. Decode, group by )
+
+-- 32. EQUI 조인을 사용하여 SCOTT 사원의 부서번호와 부서 이름을 출력하시오.
+
+-- 33. INNER JOIN과 ON 연산자를 사용하여 사원 이름과 함께 그 사원이 소속된 부서이름과 지역 명을 출력하시오.
+
+-- 34. INNER JOIN과 USING 연산자를 사용하여 10번 부서에 속하는 모든 담당 담당 업무의 고유 목록(한 번씩만 표시)을 부서의 지역명을 포함하여 출력 하시오.
+
+-- 35. NATURAL JOIN을 사용하여 커미션을 받는 모든 사원의 이름, 부서이름, 지역 명을 출력하시오.
+
+-- 36. EQUI 조인과 WildCARD를 사용하여 이름에 ‘A’가 포함된 모든 사원의 이름과 부서명을 출력하시오.
+
+-- 37. NATUAL JOIN을 이용하여 NEW YORK에 근무하는 모든 사원의 이름, 업무, 부서번호 및 부서명을 출력하시오.
+
+-- 38. SELF JOIN을 사용하여 사원의 이름 및 사원번호를 관리자 번호와 함께 출력하시오.
+
+-- 39. OUTER JOIN, SELF JOIN을 사용하여 관리자가 없는 사원을 포함하여 사원번호를 기준으로 내림차순 정렬하여 출력하시오.
+
+-- 40. SELF JOIN을 사용하여 지정한 사원의 이름, 부서번호, 지정한 사원과 동일한 부서에서 근무하는 사원을 출력하시오. ( SCOTT )
+
+-- 41. SELF JOIN을 사용하여 WARD 사원보다 늦게 입사한 사원의 이름과 입사일을 출력하시오
+
+-- 42. SELF JOIN 을 사용하여 관리자보다 먼저 입사한 모든 사원의 이름 및 입사일을 관리자의 이름 및 입사일과 함께 출력하시오.
+
+-- 43. 사원 번호가 7788인 사원과 담당 업무가 같은 사원을 표시(사원 이름과 담당업무)하시오.
+
+-- 44. 사원번호가 7499인 사원보다 급여가 많은 사원을 표시하시오. 사원이름과 담당 업무구하
+
+-- 45. 최소급여를 받는 사원의 이름, 담당업무 및 급여를 표시하시오. (그룹함수 사용)
+
+-- 46. 평균급여가 가장 적은 직급의 직급 이름과 직급의 평균을 구하시오.
+
+-- 47. 각 부서의 최소 급여를 받는 사원의 이름, 급여, 부서번호를 표시하시오.
+
+-- 48. 담당업무가 ANALYST 인 사원보다 급여가 적으면서 업무가 ANALYST가 아닌 사원들을 표시(사원번호, 이름, 담당 업무, 급여)하시오.
+
+-- 49. 부하직원이 없는 사원의 이름을 표시하시오.
+
+-- 50. 부하직원이 있는 사원의 이름을 표시하시오.
+
+-- 51. BLAKE와 동일한 부서에 속한 사원의 이름과 입사일을 표시하는 질의를 작성하시오.   ( 단 BLAKE는 제외 )
+
+-- 52. 급여가 평균 급여보다 많은 사원들의 사원 번호와 이름을 표시하되 결과를 급여에 대해서 오름차순으로 정렬하시오.
+
+-- 53. 이름에 K가 포함된 사원과 같은 부서에서 일하는 사원의 사원 번호와 이름을 표시하시오.
+
+-- 54. 부서위치가 DALLAS인 사원의 이름과 부서번호 및 담당업무를 표시하시오.
+
+-- 55. KING에게 보고하는 사원의 이름과 급여를 표시하시오.
+
+-- 56. RESEARCH 부서의 사원에 대한 부서번호, 사원이름 및 담당 업무를 표시하시오.
+
+-- 57. 평균 월급보다 많은 급여를 받고 이름에 M이 포함된 사원과 같은 부서에서 근무하는 사원의 사원 번호, 이름, 급여를 표시하시오.
+
+-- 58. 평균급여가 가장 적은 업무를 찾으시오.
+
+-- 59. 담당업무가 MANAGER 인 사원이 소속된 부서와 동일한 부서의 사원을 표시하시오.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
